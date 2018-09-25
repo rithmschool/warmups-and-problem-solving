cdaa4968e73a6b37c9835fe498ebd22485e1c2fe
annabrit <annabrit@gmail.com>
2018-09-24 17:16:44 -0700

    fixing test callback functions in 15-partition

--- python-fundamentals/15-partition/test.py
+++ python-fundamentals/15-partition/test.py
@@ -5,19 +5,19 @@ from partition import partition
 
 class UnitTests(unittest.TestCase):
 
-    def isEven(num):
-        return num % 2 == 0
-
     def test_input_1(self):
+        def isEven(num):
+            return num % 2 == 0
         # Failure message:
         # expected partition([1, 2, 3, 4], isEven)
         # to equal [ [ 2, 4 ], [ 1, 3 ] ]
         self.assertEqual(partition([1, 2, 3, 4], isEven), [[2, 4], [1, 3]])
 
-    def isString(el):
-        return isinstance(el, str)
 
     def test_input_2(self):
+
+        def isString(el):
+            return isinstance(el, str)
         # Failure message:
         # expected partition(["hi", None, 6, "bye"], isString)
         # to equal [ [ "hi", "bye" ], [ None, 6 ] ]
